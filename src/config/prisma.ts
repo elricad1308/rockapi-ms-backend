2d057c6e12970a26a87fa0736fa174c506723cbb
José Aguilar <jc.aguilar1308@gmail.com>
2026-05-01 13:44:12 -0600

    feat: :wrench: Initial Prisma database config.

---
+++ src/config/prisma.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { PrismaClient } from '../../generated/prisma/client';
+
+const host: string = process.env.DATABASE_HOST ?? "";
+const user: string = process.env.DATABASE_USER ?? "";
+const pass: string = process.env.DATABASE_PASSWORD ?? "";
+const name: string = process.env.DATABASE_NAME ?? "";
+
+const adapter = new PrismaMariaDb({
+  host: host,
+  user: user,
+  password: pass,
+  database: name,
+  connectionLimit: 5
+});
+
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
